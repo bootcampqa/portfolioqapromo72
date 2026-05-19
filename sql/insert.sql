498d23aec083259102dbc653faf8ae66a607d8a8
Reyes Cuesta Bootcamp QA <154596444+bootcampqa@users.noreply.github.com>
2026-05-19 19:40:15 +0200

    Update SQL insert statement for FormReyesCuesta

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,4 +1,6 @@
-INSERT INTO form (name, email, message, age)
-VALUES
-('Ana','ana@gmail.com' , 'test message', NULL),
-('Luis','luis@gmail.com' , 'hello world', 28);
+INSERT INTO FormReyesCuesta (name, email, subject, age, message) values
+('ana', 'ana@gmail.com', 'information', NULL, 'hola testing'),
+(NULL, 	'pedro@gmail.com', 'job', 20, 'pedro mensaje'),
+('maria', 'maria@yahoo.com', 'other', 25, 'mensaje de prueba'),
+('ana', 'ana.work@gmail.com', 'job', 30, 'test message'),
+(NULL, 'user1@gmail.com', 'information', 20, 'mensaje testing');
